890450e9093c2ec0f9357d295aab82386897cbce
Nariman <onlanovnar@gmail.com>
2025-05-28 20:14:54 +0500

    Add auth page buttons

--- lib/config/themes/app_theme.dart
+++ lib/config/themes/app_theme.dart
@@ -5,5 +5,44 @@ class AppTheme {
   static final lightTheme = ThemeData(
     fontFamily: 'IBM Plex Sans',
     scaffoldBackgroundColor: AppColors.secondary,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        foregroundColor: AppColors.secondary,
+        textStyle: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w700,
+        ),
+        minimumSize: const Size(double.infinity, 60),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 20,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+      ),
+    ),
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+        side: const BorderSide(
+          color: AppColors.secondary,
+          width: 1,
+        ),
+        foregroundColor: AppColors.secondary,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 20,
+        ),
+        minimumSize: const Size(double.infinity, 60),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        textStyle: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+    ),
   );
 }
--- lib/features/presentation/pages/auth/auth_page.dart
+++ lib/features/presentation/pages/auth/auth_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smart_pot_front/config/assets/app_images.dart';
+import 'package:smart_pot_front/config/themes/app_colors.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
@@ -8,12 +9,53 @@ class AuthPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.all(25),
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage(
               AppImages.authBg,
             ),
+            fit: BoxFit.cover,
           ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Spacer(),
+            Text(
+              'Ухаживай за растениями просто!',
+              style: TextStyle(
+                color: AppColors.secondary,
+                fontSize: 28,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Здесь ты можешь заботиться о растении вне дома',
+              style: TextStyle(
+                color: AppColors.secondary,
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Логин'),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text('Регситрация'),
+            ),
+          ],
         ),
       ),
     );
